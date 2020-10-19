882b79297a7f89af656c84e580c53982b205c954
Juanmagra <juanmagra@github.com>
2020-10-19 07:26:39 +0200

    Algo de seguridad

--- GraciaPardal_JuanManuel/src/main/java/com/salesianostriana/dam/GraciaPardal_JuanManuel/GraciaPardalJuanManuelApplication.java
+++ GraciaPardal_JuanManuel/src/main/java/com/salesianostriana/dam/GraciaPardal_JuanManuel/GraciaPardalJuanManuelApplication.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@SpringBootApplication()
 public class GraciaPardalJuanManuelApplication {
 
 	public static void main(String[] args) {

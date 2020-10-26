312ca6ede069c5c2f53338eb6e0487e404009ee4
Juanmagra <juanmagra@github.com>
2020-10-26 18:14:20 +0100

    Paginación implementada con exito

--- GraciaPardal_JuanManuel/src/main/java/com/salesianostriana/dam/GraciaPardal_JuanManuel/controller/PublicController.java
+++ GraciaPardal_JuanManuel/src/main/java/com/salesianostriana/dam/GraciaPardal_JuanManuel/controller/PublicController.java
@@ -1,17 +1,22 @@
 package com.salesianostriana.dam.GraciaPardal_JuanManuel.controller;
 
+import com.salesianostriana.dam.GraciaPardal_JuanManuel.model.Categoria;
 import com.salesianostriana.dam.GraciaPardal_JuanManuel.model.Usuario;
+import com.salesianostriana.dam.GraciaPardal_JuanManuel.service.CategoriaServi;
 import com.salesianostriana.dam.GraciaPardal_JuanManuel.service.ProductoServi;
 import com.salesianostriana.dam.GraciaPardal_JuanManuel.service.UsuarioServi;
 import lombok.AllArgsConstructor;
+import org.aspectj.apache.bcel.classfile.Module;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @Controller
 @AllArgsConstructor
@@ -20,14 +25,16 @@ public class PublicController {
 
     private final ProductoServi productoServi;
     private final UsuarioServi usuarioServi;
+    private final CategoriaServi categoriaServi;
 
     //Listar producto
     @GetMapping("/")
-    public String listarProductos(Model model, @AuthenticationPrincipal Usuario user){
-        String url="Index";
-        model.addAttribute("lista", productoServi.findAll());
+    public String listarProductos(Model model, Pageable pageable){
 
-        return url;
+
+        model.addAttribute("lista", productoServi.findAllPaginated(pageable));
+
+        return "Index";
 
     }
 
--- GraciaPardal_JuanManuel/src/main/java/com/salesianostriana/dam/GraciaPardal_JuanManuel/repository/ProductoRepo.java
+++ GraciaPardal_JuanManuel/src/main/java/com/salesianostriana/dam/GraciaPardal_JuanManuel/repository/ProductoRepo.java
@@ -1,10 +1,17 @@
 package com.salesianostriana.dam.GraciaPardal_JuanManuel.repository;
 
 
+import com.salesianostriana.dam.GraciaPardal_JuanManuel.model.Categoria;
 import com.salesianostriana.dam.GraciaPardal_JuanManuel.model.Producto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductoRepo extends JpaRepository <Producto, Long> {
+
+    Page<Producto> findByCategoria(Pageable pageable, Categoria categoria);
 }
